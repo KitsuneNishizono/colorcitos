4a92cba397df67fb1b51da039fd25cc44d4f20a6
Kitsune Nishizono <rrangel@hub-camp.mx>
2018-02-18 23:55:45 -0600

    uso de js para colores random

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+var my_box = document.getElementById("colorbox");
+  var r = parseInt(Math.random() * 255);
+  var g = parseInt(Math.random() * 255);
+  var b = parseInt(Math.random() * 255);
+return "rgb("r","g","b")";
+my_box.style.backgroundColor = ‘rgb()’;
